873e347d94590ec6ef78d934e2c8c36757aeebde
Mauricio Muñoz <gmauricio.munoz@gmail.com>
2018-10-11 09:14:42 -0500

    feat: create minion army

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # minion-army
 Microservice Framework for RabbitMQ Workers
+
+## usage
+
+```javascript
+const Army = require('@pager/minion-army');
+
+const manifest = {
+    connection: { // optional, if not provided will default to a rabbit connection to local host
+        rabbitUrl: 'amqp://localhost'
+    },
+    defaults: { // default values that apply for all workers
+        exchangeName: 'my-exchange-name'
+    },
+    workers: [
+        {
+            handler: (message) => console.log('my job is to log this', message),
+            config: { // same config as expected by minions
+                name: 'jobs.logging',
+                key: 'events.something.happened'
+            }
+        },
+        {
+            handler: (message) => true,
+            config: {
+                name: `jobs.trueing`,
+                key:  `events.something.happened`
+            }
+        }
+    ]
+};
+
+const army = Army(manifest);
+
+army.start(); // if you provide defaults.autoStart = true this is not needed
+```
--- test/index.js
+++ test/index.js
@@ -1,5 +1,43 @@
 const test = require('ava')
+const Army = require('../lib/index')
 
-test('acks simple handler', async t => {
-    t.true(true)
+test('Creates army from manifest and workers work', async t => {
+  const manifest = {
+    connection: {
+      rabbit: {
+        topic: () => ({
+          publish: () => {}
+        })
+      }
+    },
+    defaults: {
+      exchangeName: 'my-exchange-name'
+    },
+    workers: [
+      {
+        handler: (message) => message,
+        config: {
+          name: 'logging',
+          key: 'events.something.happened'
+        }
+      },
+      {
+        handler: (message) => true,
+        config: {
+          name: 'trueing',
+          key: 'events.something.happened'
+        }
+      }
+    ]
+  }
+
+  const army = Army(manifest)
+
+  t.truthy(army)
+  t.truthy(army.minions)
+  t.truthy(army.minions['logging'])
+  t.truthy(army.minions['trueing'])
+
+  t.is(await army.minions['logging'].handle('hola'), 'hola')
+  t.is(await army.minions['trueing'].handle('hola'), true)
 })
